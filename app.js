1b61acbbdb58bd44266019110b8b97873b60e3d4
robbobfrh84 <bobmain49@gmail.com>
2023-07-20 13:51:31 -0700

    lasypush

---
+++ app.js
@@ -0,0 +1,54 @@
+let URL = "https://palm-ai-app.uw.r.appspot.com/"
+
+window.onload = ()=>{
+  console.log(window.location)
+  const hash = window.location.hash
+  if (hash === "#dev") {
+    URL = "http://localhost:8080/"
+  }
+  thingInput.focus()
+}
+window.reload = ()=>{
+  thingInput.focus()
+}
+
+document.body.addEventListener("keypress", function(event) {
+  if (event.key === "Enter" && thingInput.value != "") {
+    event.preventDefault();
+    get()
+  } else {
+    console.log('no input')
+  }
+});
+
+
+function get(){
+  const input = thingInput.value
+  fetch(URL+input)
+    .then( res => res.json())
+    .then( data => displayColors(data))
+}
+
+function displayColors({ validOutputs, colorObjectArray }) {
+  console.log('validOutputs, colorObjectArray:',validOutputs, colorObjectArray)
+  if (validOutputs.length > 1) {
+    colorNames.innerHTML = ""
+    validOutputs.forEach(c=>{
+      let rgb = colorObjectArray.filter(o=>o.name === c)[0]?.rgb
+      colorNames.innerHTML += /*html*/`
+        <div class="colorCircleContainer">
+          <div class='colorName'>${c}</div>
+          <div 
+            class='colorCircle' id="${c}" 
+            style="background-color: ${rgb || ''};"
+          ></div>
+        </div>
+      `
+      if (!rgb && c === "transparent") {
+        console.log("TR")
+        console.log('window[c]:',window[c])
+        window[c].style = "background: url('assets/transparent.png');"
+      }
+    })
+  } else { alert("something went wrong")}
+}
