ec31c93f4d077e1d9ed7657856919941f665b099
robbobfrh84 <bobmain49@gmail.com>
2023-08-09 14:10:22 -0700

    lasypush

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let _URL = "http://localhost:8080/"
 let _loaderOn = false
 let _theme = 'light'
+let _requestInProgress = false
 let _req_config = { colors: 6, requests: 2 }
 
 window.onload = ()=>{
@@ -10,11 +11,11 @@ window.onload = ()=>{
   if ( clientURL === "https://robbobfrh84.github.io/palm-ai-app/" || hash === "#prod") {
     _URL = "https://palm-ai-app.uw.r.appspot.com/"
   }
-  // thingInput.focus()
+  thingInput.focus()
 }
 
 window.reload = ()=>{
-  // thingInput.focus()
+  thingInput.focus()
 }
 
 document.body.addEventListener("keypress", function(event) {
@@ -32,33 +33,45 @@ lightDarkToggle.onchange = ()=>{
   document.documentElement.style.setProperty('--themeB', 'var(--'+themeA+')');
 }
 
+function checkLoaderOff(requestCnt, lastDelay) {
+  if ( requestCnt >= _req_config.requests ) {
+    const resultColorTransition = getComputedStyle(document.documentElement).getPropertyValue('--resultColorTransition')
+    const loaderDelay = lastDelay + (resultColorTransition.split('s')[0] * 1000) 
+    setTimeout(()=>{ 
+      turnOffLoader() 
+      toggleButtonsDisabled(false)
+      _requestInProgress = false
+    }, loaderDelay) 
+  }
+}
+
+function toggleButtonsDisabled(status) {
+  enterButton.disabled = status;
+  thingInput.disabled = status;
+}
+
 
 /* - - - - - API - - - - - */ 
-// function UIGet(thing) { 
-//   thingInput.value = thing
-//   get(thing)
-// }
-
-function get(thing, resultsObj){
-  if (thing) { thingInput.value = thing }
-  colorsResult.innerHTML = ""
-  turnOnLoader()
-  const request = {
-    thing: thing || thingInput.value,
-    colors: _req_config.colors,
-    requests: _req_config.requests
+function get(thing, resultsObj, keep){
+  if (!_requestInProgress || keep) {
+    _requestInProgress = true
+    if (thing) { thingInput.value = thing }
+    if (!keep) { clearAllChildren(colorsResult) }
+    toggleButtonsDisabled(true)
+    turnOnLoader()
+    const request = {
+      thing: thing || thingInput.value,
+      colors: _req_config.colors,
+      requests: _req_config.requests
+    }
+    const urlString = _URL+request.thing+"/"+request.colors
+    fetch(urlString)
+      .then( res => res.json())
+      .then( data => handleColors(data, request, resultsObj))
+      .catch( error => { 
+        _loaderOn = false
+        turnOffLoader()
+        console.log('🚨 error:', error) // * error needs to log to show any code error after this point. 
+      }) // * .finally( ()=> ... )
   }
-  const urlString = _URL+request.thing+"/"+request.colors
-
-
-
-  fetch(urlString)
-    .then( res => res.json())
-    .then( data => handleColors(data, request, resultsObj))
-    .catch( error => { 
-      _loaderOn = false
-      turnOffLoader()
-      console.log('🚨 error:', error) // * error needs to log to show any code error after this point. 
-    }) 
-    // .finally( ()=> ... )
 }
