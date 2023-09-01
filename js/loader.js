90581ff56f1949b3008e3ec0fe01576b634fee21
robbobfrh84 <bobmain49@gmail.com>
2023-09-01 11:55:43 -0700

    lasypush

--- js/loader.js
+++ js/loader.js
@@ -1,11 +1,40 @@
 // 🟢... 🟡... 🔴...  LOADER
 function turnOnLoader() {
   _loaderOn = true
-  loader.innerHTML = "&nbsp; &nbsp; ...Loading"
+  enterButton.style.opacity = 0
+  setTimeout(()=>{
+    _loader.on("loader1")
+  },300)
 }
 
 function turnOffLoader(check) {
   if (!_loaderOn || !check) {
-    loader.innerHTML = ""
+    _loader.off("loader1")
+    setTimeout(()=>{
+      enterButton.style.opacity = 1
+    },800)
   }
 }
+
+const _loader = {
+  state: "off",
+  on: function(id) {
+    window[id].style.display = "block"
+    window[id].style.opacity = 1
+    this.state = "on"
+  },
+  off: function(id) {
+    window[id].style.opacity = 0
+    this.state = "off"
+    setTimeout(()=>{
+      if (this.state == "off") {
+        window[id].style.display = "none"
+      }
+      else {
+        window[id].style.opacity = 1
+        window[id].style.display = "block"
+      }
+    }, 1000)
+  }
+}
+
