69f0054881f4b75f753cd59674d25ee3406bbfc7
Bob Main <bobmain49@gmail.com>
2024-10-04 19:03:23 -0700

    lasypush

--- server/app/ai.js
+++ server/app/ai.js
@@ -1,7 +1,3 @@
-// const { TextServiceClient } = require("@google-ai/generativelanguage");
-// const { GoogleAuth } = require("google-auth-library");
-
-
 const {
   GoogleGenerativeAI,
   HarmCategory,
@@ -30,69 +26,24 @@ const generationConfig = {
   responseMimeType: "application/json",
 };
 
-
-// const MODEL_NAME = "models/text-bison-001";
-
-// const client = new TextServiceClient({
-//   authClient: new GoogleAuth().fromAPIKey(API_KEY),
-// });
-
-// const stopSequences = [];
-
 const reqTextPrompt = function( thing, colors ) {
 
   const promptString = getPromptString( thing, colors );
 
-  // async function run() {
   return new Promise(resolve => {
     const chatSession = model.startChat({
       generationConfig,
-   // safetySettings: Adjust safety settings
-   // See https://ai.google.dev/gemini-api/docs/safety-settings
-      history: [
-      ],
+      history: [],
     });
   
     chatSession.sendMessage(promptString).then(result => {
       const outputs = JSON.parse(result.response.text())
-      // const outputs = output.split('"').join("").split(", ")
-      // console.log('outputs:',outputs)
-      // * outputs.push("badoutput")
       const { validOutputs, invalidOutputs } = isValidColors(outputs)
       resolve({ outputs, invalidOutputs, validOutputs })
     });
   })
 }
   
-  // run();
-
-//   const promptString = getPromptString( thing, colors )
-
-//   return new Promise(resolve => {
-//     client.generateText({
-//       model: MODEL_NAME, // required, which model to use to generate the result
-//       temperature: 0.35, // optional, 0.0 always uses the highest-probability result
-//       candidateCount: 1, // optional, how many candidate results to generate
-//       top_k: 40, // optional, number of most probable tokens to consider for generation
-//       top_p: 0.95, // optional, for nucleus sampling decoding strategy
-//       max_output_tokens: 1024, // optional, maximum number of output tokens to generate
-//       stop_sequences: stopSequences, // optional, sequences at which to stop model generation
-//       // * optional, safety settings
-//       safety_settings: [{"category":"HARM_CATEGORY_DEROGATORY","threshold":1},{"category":"HARM_CATEGORY_TOXICITY","threshold":1},{"category":"HARM_CATEGORY_VIOLENCE","threshold":2},{"category":"HARM_CATEGORY_SEXUAL","threshold":2},{"category":"HARM_CATEGORY_MEDICAL","threshold":2},{"category":"HARM_CATEGORY_DANGEROUS","threshold":2}],
-//       prompt: {
-//         text: promptString,
-//       },
-//     }).then(result => {
-//       // * console.log('result:',JSON.stringify(result, null, 2))
-//       const output = JSON.stringify(result[0].candidates[0].output, null, 2)
-//       const outputs = output.split('"').join("").split(", ")
-//       // * outputs.push("badoutput")
-//       const { validOutputs, invalidOutputs } = isValidColors(outputs)
-//       resolve({ outputs, invalidOutputs, validOutputs })
-//     });
-//   })
-// }
-
 function isValidColors(outputs) {
   const validOutputs = []
   const invalidOutputs = []
